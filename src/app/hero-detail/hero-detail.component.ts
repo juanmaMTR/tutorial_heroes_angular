117bf0c67433b18c0b8afaf7b499385721491a20
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-02-15 14:06:42 +0100

    Tutorial casi terminado

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,7 +11,11 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
   @Input() heroe?: Hero;
-  constructor() { }
+  constructor(
+    private route:ActivatedRoute,
+    private servicioHeroe:HeroService,
+    private localizacion:Location
+  ) { }
 
   ngOnInit(): void {
   }
