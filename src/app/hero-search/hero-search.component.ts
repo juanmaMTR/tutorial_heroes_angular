18544fd4b3f830bb447308b753c8ca34d20bb8fd
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-02-17 13:26:58 +0100

    Tutorial heroes terminado

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { from, Observable, Subject } from 'rxjs';
+import { debounceTime,distinctUntilChanged,switchMap } from 'rxjs/operators';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-search',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
-
-  constructor() { }
+  heroes$!:Observable<Hero[]>
+  private buscarTerminos=new Subject<string>()
+  constructor(private servicioHeroe:HeroService) { }
 
   ngOnInit(): void {
+    this.heroes$=this.buscarTerminos.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((termino:string)=>this.servicioHeroe.buscarHeroes(termino))
+    )
+  }
+
+  buscar(termino:string):void{
+    this.buscarTerminos.next(termino)
   }
 
 }
